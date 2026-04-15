10a940440a0e1a9a898cfe67f38326bda6558bf0
yy <yy@example.com>
2026-04-15 21:41:07 +0800

    清理 springboot 相关模块代码

--- springboot-netty/src/main/java/com/wx/wxfx/client/Disposable.java
+++
@@ -1,6 +0,0 @@
-package com.wx.wxfx.client;
-
-public interface Disposable {
-    void dispose();
-}
-
--- springboot-webflux/src/main/java/com/wx/entity/CustomizeException.java
+++
@@ -1,7 +0,0 @@
-package com.wx.entity;
-
-public class CustomizeException extends RuntimeException{
-    public CustomizeException(String message) {
-        super(message);
-    }
-}
