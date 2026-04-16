08faff17a5fe810db8ffed0f076fcf0bb33a9600
yy <yy@example.com>
2026-04-16 19:44:53 +0800

    feat: create algorithm module

---
+++ modules/algorithm/src/main/java/com/wx/study/algorithm/sort/Insert.java
@@ -0,0 +1,26 @@
+package com.wx.study.sort;
+
+/**
+ * 插入排序
+ * 对于1-N-1之间的每一个i，将a[i]与a[0]到a[i-1]中比它小的所有元素一次有序地交换
+ * 在索引i 由左向右变化的过程中，它左侧的元素总是有序的，所以当i到达数组的右端时
+ * 排序就完成了。
+ *
+ * @author wxli
+ * @date 2021/8/10 22:50
+ */
+public class Insert extends AbstractSort {
+
+    @Override
+    public void sort(Comparable[] a) {
+        //将a[] 按照升序排列
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                //依次交换
+                exch(a, j, j - 1);
+            }
+            show(a, i);
+        }
+    }
+}
---
+++ modules/algorithm/src/main/java/com/wx/study/algorithm/sort/Quick3way.java
@@ -0,0 +1,30 @@
+package com.wx.study.sort;
+
+/**
+ * 三向切分快速排序
+ *
+ * @author wxli
+ * @date 2021/8/12 23:38
+ */
+public class Quick3way extends AbstractSort {
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+        if (hi <= lo) return;
+        int lt = lo, i = lo + 1, gt = hi;
+        Comparable v = a[lo];
+        while (i < gt) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) exch(a, lt++, i++);
+            else if (cmp > 0) exch(a, i, gt--);
+            else i++;
+        }
+        sort(a, lo, lt - 1);
+        sort(a, gt + 1, hi);
+    }
+
+    @Override
+    public void sort(Comparable[] a) {
+        int lo = 0, hi = a.length - 1;
+        sort(a, lo, hi);
+    }
+}
